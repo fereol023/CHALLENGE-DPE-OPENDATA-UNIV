f5c1400f9600aa0b15de785a1c3a53130d308a06
Féréol Gbenou <gbenoufereol@yahoo.com>
2025-01-30 07:22:28 +0100

    updates selection de variables

--- utils/fonctions.py
+++ utils/fonctions.py
@@ -24,6 +24,44 @@ def compute_entropies(d, cols):
     return entropies      
 
 
+def get_cross_entropy(pk, qk):
+    """
+    Calcule l'entropie croisée entre deux distributions de probabilité pk et qk.
+    
+    :param pk: np.array, distribution réelle (P, par ex. probabilité des classes).
+    :param qk: np.array, distribution prédite (Q, estimée par un modèle).
+    :return: float, valeur de l'entropie croisée.
+    """
+    epsilon = 1e-15
+    qk = np.clip(qk, epsilon, 1 - epsilon)  # Évite log(0)
+    return -np.sum(pk * np.log(qk))
+
+def compute_cross_entropies(d, col_pairs):
+    """
+    Calcule les entropies croisées pour des paires de colonnes dans un dataframe.
+    
+    :param d: pd.DataFrame, dataframe contenant les données.
+    :param col_pairs: list of tuples, chaque tuple (col_P, col_Q) représente une paire 
+                      de colonnes pour lesquelles calculer l'entropie croisée.
+    :return: list, entropies croisées par paire de colonnes.
+    """
+    print('Computing cross-entropies...')
+    cross_entropies = []
+    
+    for col_P, col_Q in col_pairs:
+
+        # Alignement des distributions (au cas où les valeurs diffèrent)
+        unique_values = sorted(set(d[col_P].dropna().unique()) | set(d[col_Q].dropna().unique()))
+        pk_full = np.array([d[col_P].value_counts(normalize=True, dropna=False).get(v, 0) for v in unique_values])
+        qk_full = np.array([d[col_Q].value_counts(normalize=True, dropna=False).get(v, 0) for v in unique_values])
+
+        # Calcul de l'entropie croisée
+        cross_entropy = round(get_cross_entropy(pk_full, qk_full), 4)
+        cross_entropies.append({"col_pair": (col_P, col_Q), "cross_entropy": cross_entropy})
+    
+    return cross_entropies
+
+
 @lru_cache(maxsize=1024)
 def normalize_name(colname):
     pat1, pat2 = re.compile('[^0-9a-zA-Z]+'), re.compile('_+')
